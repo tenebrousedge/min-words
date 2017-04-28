ed368de0968ddb6c5af52c8fc505781c181c2828
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-28 13:47:19 -0700

    add backend and tests
    The astute observer will note that the bulk of the working parts here have been committed in one go
    I do try to make an effort to write tests first, but it's pretty much nonsense for anything remotely complicated
    See:
    http://david.heinemeierhansson.com/2014/tdd-is-dead-long-live-testing.html

--- lib/min_words.rb
+++ lib/min_words.rb
@@ -1,5 +1,91 @@
-require "min_words/version"
+require 'min_words/version'
+require 'sequel'
+require 'sqlite3'
 
+# Namespace module for MinWords
+#
+# @author Kai Leahy
 module MinWords
-  # Your code goes here...
+  # In-memory database initialization
+  DB = Sequel.sqlite
+
+  # Sequel class referencing the words table
+  class Word < Sequel::Model
+    one_to_many :defines
+  end
+
+  # Sequel class referencing the definitions table
+  class Define < Sequel::Model
+    many_to_one :word
+  end
+
+  # Provides methods for storing and retrieving words and definitions
+  class Dictionary
+    # initialize the database upon instantiation
+    def initialize
+      create_tables
+    end
+
+    # save a word and definition to the database
+    #
+    # @param [Hash<String>] word
+    # @param [Hash<String>] definition
+    def save_word(word, definition)
+      id = MinWords::DB[:words].insert word
+      MinWords::DB[:defines].insert definition.merge(word_id: id)
+    end
+
+    # returns a specific word's definitions
+    #
+    # @param [String] word The word to find, as a string
+    def find(word)
+      MinWords::DB[:words]
+      .where(:word_text => word)
+      .left_join(:defines, :word_id => :id)
+      .first
+    end
+
+    # returns all words and their ids
+    def findAll
+      MinWords::DB[:words]
+      .left_join(:defines, :word_id => :id)
+      .all
+    end
+
+    # Updates a word's definition
+    #
+    # #param []
+    def update_definition(id, new_def)
+      MinWords::DB[:defines]
+      .where(id: id)
+      .update(:definition_text => new_def)
+    end
+
+    def create_tables
+      unless MinWords::DB.table_exists?(:words)
+        MinWords::DB.create_table :words do
+          primary_key :id
+          varchar :word_text
+        end
+      end
+      unless MinWords::DB.table_exists?(:defines)
+        MinWords::DB.create_table :defines do
+          primary_key :id
+          foreign_key :word_id, :words
+          varchar :definition_text
+        end
+      end
+    end
+
+    def clear_tables
+      MinWords::DB.drop_table :defines, :words
+      create_tables
+    end
+  end
+end
+
+class Hash
+  def keys_to_symbol
+    Hash[self.map { |k,v| [k.to_sym, v.is_a?(Hash) ? v.keys_to_symbol : v] }]
+  end
 end
--- spec/min_words_spec.rb
+++ spec/min_words_spec.rb
@@ -1,11 +1,68 @@
 require "spec_helper"
+require 'pry'
 
 RSpec.describe MinWords do
   it "has a version number" do
     expect(MinWords::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe MinWords::Dictionary do
+    it "initializes database tables" do
+      dict = MinWords::Dictionary.new
+      expect(MinWords::DB).to be_a Sequel::Database
+      expect(MinWords::DB[:words].columns).to match_array([:id, :word_text])
+      expect(MinWords::DB[:defines].columns).to match_array([:id, :word_id, :definition_text])
+    end
+
+    describe 'MinWords::Dictionary#clear_tables' do
+      it "deletes database tables" do
+        @dict = MinWords::Dictionary.new
+        @dict.clear_tables
+        expect(MinWords::DB[:words].empty?).to eq(true)
+        expect(MinWords::DB[:defines].empty?).to eq(true)
+      end
+    end
+
+    describe 'MinWords::Dictionary#save_word' do
+      before() do
+        @dict = MinWords::Dictionary.new
+        @dict.clear_tables
+      end
+      it "saves words to the database" do
+        word = {word_text: 'meow'}
+        definition = {definition_text: 'a shocking word, never to be used.'}
+        expect(@dict.save_word(word, definition)).to(eq(1))
+      end
+    end
+
+    describe 'MinWords::Dictionary#find' do
+      before() do
+        @dict = MinWords::Dictionary.new
+        word = {word_text: 'meow'}
+        definition = {definition_text: 'a shocking word, never to be used.'}
+        @dict.clear_tables
+        @dict.save_word(word, definition)
+      end
+
+      it "finds words in the dictionary" do
+        e = {:id=>1, :word_text=>"meow", :word_id=>1, :definition_text=>"a shocking word, never to be used."}
+        expect(@dict.find('meow')).to(eq(e))
+      end
+    end
+
+    describe 'MinWords::Dictionary#findAll' do
+        before() do
+        word = {word_text: 'meow'}
+        definition = {definition_text: 'a shocking word, never to be used.'}
+        @dict = MinWords::Dictionary.new
+        @dict.clear_tables
+        @dict.save_word(word, definition)
+      end
+
+      it "finds all words and definitions" do
+        e = [{:id=>1, :word_text=>"meow", :word_id=>1, :definition_text=>"a shocking word, never to be used."}]
+        expect(@dict.findAll).to(eq(e))
+      end
+    end
   end
 end
